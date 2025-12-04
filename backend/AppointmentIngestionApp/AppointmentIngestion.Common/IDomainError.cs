7587aa43b6b8f4b86b6ec4eb8e39a83e7233d7f9
ts <ts@artificialcore.com>
2025-12-04 17:55:32 +0400

    common layer added for resulting

---
+++ backend/AppointmentIngestionApp/AppointmentIngestion.Common/IDomainError.cs
@@ -0,0 +1,9 @@
+﻿namespace AppointmentIngestion.Common
+{
+    public interface IDomainError
+    {
+        string? ErrorMessage { get; init; }
+        ErrorType ErrorType { get; init; }
+        public List<string>? Errors { get; init; }
+    }
+}
