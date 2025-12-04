29d87c4d8b7d5cef6c3214d01c40e6b43935ab73
ts <ts@artificialcore.com>
2025-12-04 22:47:27 +0400

    responsedtodata moved to a separate class

--- backend/AppointmentIngestionApp/tests/AppointmentIngestion.Tests.Data/AppointmentRequestDtoData.cs
+++ backend/AppointmentIngestionApp/tests/AppointmentIngestion.Tests.Data/AppointmentRequestDtoData.cs
@@ -1,22 +1,9 @@
-﻿using AppointmentIngestion.Repositories.Models;
-using AppointmentIngestion.Services.DTOs;
-
-namespace AppointmentIngestion.Tests.Data
+﻿namespace AppointmentIngestion.Tests.Data
 {
     public static class AppointmentRequestDtoData
     {
         public const string ValidCustomer = "Rowan Miller";
         public static DateTime ValidAppointmentTime = DateTime.UtcNow.AddMinutes(10);
         public static int ValidServiceDurationMinutes = 10;
-    }
-
-    public static class AppointmentResponseDtoData
-    {
-        public static List<AppointmentResponseDto> ValidAppointments
-         = new List<AppointmentResponseDto>
-         {
-                new AppointmentResponseDto { Id = 1 },
-                new AppointmentResponseDto { Id = 2 }
-         };
     }
 }
---
+++ backend/AppointmentIngestionApp/tests/AppointmentIngestion.Tests.Data/AppointmentResponseDtoData.cs
@@ -0,0 +1,14 @@
+﻿using AppointmentIngestion.Services.DTOs;
+
+namespace AppointmentIngestion.Tests.Data
+{
+    public static class AppointmentResponseDtoData
+    {
+        public static List<AppointmentResponseDto> ValidAppointments
+         = new List<AppointmentResponseDto>
+         {
+                new AppointmentResponseDto { Id = 1 },
+                new AppointmentResponseDto { Id = 2 }
+         };
+    }
+}
